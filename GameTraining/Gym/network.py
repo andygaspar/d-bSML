optimizer and softmax fix

GameTraining/Gym/network.py
@@ -34,14 +34,19 @@ class Network:
         # print(torch.cuda.is_available())
         self.only_valid_actions = ony_valid_actions
         self.softmax = softmax
+        self.optimizer = optim.Adam(self.network.parameters(), lr=1e-4, weight_decay=1e-5)
+        #self.optimizer = optim.SGD(self.network.parameters(), lr=1e-2, momentum=0.9)
+
 
     def sample_action(self, Q_values: torch.tensor) -> int:
         if self.softmax:
-            Q_values_probabilities = Q_values.softmax(0).flatten().tolist()
-            action = np.random.choice(range(len(Q_values)), size=1, p=Q_values_probabilities)
+            Q_values = Q_values.softmax(0).flatten().numpy()
+            Q_values_probabilities = Q_values / np.sum(Q_values)
+            action = np.random.choice(range(len(Q_values_probabilities)), size=1, p=Q_values_probabilities)
             return action
         else:
             return torch.argmax(torch.flatten(Q_values)).item()
+
 
     def get_action(self, state: np.array) -> int:
         X = torch.from_numpy(state).to(self.device).reshape(1, self.inputDimension).type(dtype=torch.float32) / len(
@@ -60,9 +65,6 @@ class Network:
 
     def update_weights(self, batch: tuple, gamma: float, target_network):
         criterion = torch.nn.MSELoss()
-
-        optimizer = optim.Adam(self.network.parameters(), lr=1e-4, weight_decay=1e-5)
-        # optimizer = optim.SGD(self.network.parameters(), lr=1e-2, momentum=0.9)
 
         states, actions, nextStates, rewards = batch
         X = torch.tensor([el.tolist() for el in states]).to(self.device).reshape(len(states),
@@ -84,10 +86,13 @@ class Network:
         loss = criterion(curr_Q, expected_Q.detach())
         self.loss = loss.item()
         display.clear_output(wait=True)
-        optimizer.zero_grad()
+        self.optimizer.zero_grad()
         loss.backward()
         # torch.nn.utils.clip_grad_norm_(self.network.parameters(), 10)
-        optimizer.step()
+        self.optimizer.step()
 
     def take_weights(self, model_network):
         self.network.load_state_dict(model_network.state_dict())
+
+    def save_weights(self, filename: str):
+        torch.save(self.network.state_dict(), filename + '.pt')

GameTraining/trainingSession.py
@@ -26,7 +26,7 @@ def training_cycle(game: GameTraining, num_games: int):
         losses.append(game.players[1].model_network.loss)
         game.reset()
         if i % interval == 0:
-            print("time: ", str(int((time() - t) / 60)) + "min " + str(int(((time() - t) % 60) * 60)) + "s"
+            print("time: ", str(int((time() - t) / 60)) + "min " + str(int((time() - t) % 60)) + "s"
                   , "    ieter: ", i)
             t=time()
             print("Mean loss in previous " + str(interval) + " games ", np.mean(losses))
@@ -34,17 +34,16 @@ def training_cycle(game: GameTraining, num_games: int):
         game.players[1].update_eps(i)
 
 #ReplayMemory Params
-SAMPLE_SIZE = 3_000
-CAPACITY = 30_000
-UPDATE_STEP = 1
+SAMPLE_SIZE = 300
+CAPACITY = 1_000
 
 #
-HIDDEN = 100
-GAMMA = 0.5
+HIDDEN = 50
+GAMMA = 0.99
 
 REWARD_NO_SCORE: float = 0.5
 REWARD_SCORE: float = 10
-REWARD_OPPONENT_SCORE: float = -10
+REWARD_OPPONENT_SCORE: float = -5
 REWARD_INVALID_SCORE: float = -1000
 REWARD_SCORES_IN_ROW: float = 0
 REWARD_WIN = 50
@@ -66,8 +65,6 @@ trainer = AITrainer(2, boardsize, HIDDEN, REWARD_NO_SCORE, REWARD_SCORE, REWARD_
                     fixed_batch=FIXED_BATCH, eps_greedy_value=EPS_GREEDY_VALUE, softmax=SOFTMAX,
                     double_Q_learning=DOUBLE_Q_LEARNING)
 
-import os
-print(os.getcwd())
 players = [RandomPlayer(1, boardsize), trainer]
 trainer.replayMemory.import_memory("Gym/")
 game = GameTraining(players, boardsize)
@@ -76,7 +73,7 @@ game = GameTraining(players, boardsize)
 
 tt = time()
 training_cycle(game, NUM_GAMES)
-print("global time: ", str(int((time() - tt) / 60)) + "min " + str(int(((time() - tt) % 60) * 60)) + "s")
+print("global time: ", str(int((time() - tt) / 60)) + "min " + str(int((time() - tt) % 60)) + "s")
 
 AI = players[1].get_trained_player(1)
 
@@ -86,12 +83,12 @@ wins = 0
 
 start = time()
 
-for i in range(NUM_GAMES//100):
+for i in range(10_000):
    test_match.play()
    wins += int(test_players[1].score >= test_players[0].score)
    test_match.reset()
 
-print("win rate: ", wins / (NUM_GAMES//100))
+print("win rate: ", wins / 10_000)
 print("playing time: ", time() - start)
 
 #network_experience(game, 3_000)